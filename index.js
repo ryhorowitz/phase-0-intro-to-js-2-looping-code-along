def5eb0a92409fd27bf0ff463f855a8a4a7f724a
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-16 12:40:12 -0500

    commit tree 7535f7818e7e537a50311df0a0aca65137f539da

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+  const newArr = []
+  for ( let i = 0; i < arr.length; i++) {
+    let elem = arr[i]
+    let message = `Thank you, ${elem}, for the wonderful ${event} gift!`
+    newArr.push(message)
+
+  }
+  return newArr
+}
+
+function countDown(i) {
+  while (i >= 0) {
+    console.log(i)
+    i--
+  }
+}
